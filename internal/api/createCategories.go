2ecc8c2e4f0c73d9820c5db5f9e63cd8fc4639b4
simonwehle <simon.wehle@gmx.net>
2025-11-13 10:26:17 +0100

    refactor: moved http request to new function

--- internal/api/createCategories.go
+++ internal/api/createCategories.go
@@ -1,56 +1,21 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 
 	"serpa-cli/internal/types"
+	"serpa-cli/internal/utils"
 )
 
 func CreateCategories(baseUrl, apiVersion string, csvCategories []types.Category) ([]types.Category, error) {
-	createdCategories := make([]types.Category, 0, len(csvCategories))
-
-	for _, category := range csvCategories {
-		jsonData, err := json.Marshal(category)
-		if err != nil {
-			return nil, fmt.Errorf("error marshalling category %v: %w", category.Name, err)
-		}
-
-		url := fmt.Sprintf("%s%s/category", baseUrl, apiVersion)
-
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-		if err != nil {
-			return nil, fmt.Errorf("Error creating request: %v", err)
-		}
-		req.Header.Set("Content-Type", "application/json")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			return nil, fmt.Errorf("Error sending request: %v", err)
-		}
-
-		body, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			return nil, fmt.Errorf("Error reading response body: %v", err)
-		}
-
-		if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("API returned non-success status: %d, body: %s", resp.StatusCode, string(body))
-		}
-
-		var createdCategory types.Category
-		err = json.Unmarshal(body, &createdCategory)
-		if err != nil {
-			return nil, fmt.Errorf("Error unmarshalling response: %v", err)
-		}
-
-		createdCategories = append(createdCategories, createdCategory)
-	}
-
-	return createdCategories, nil
+    createdCategories := make([]types.Category, 0, len(csvCategories))
+    url := fmt.Sprintf("%s%s/category", baseUrl, apiVersion)
+    for _, category := range csvCategories {
+        createdCategory, err := utils.DoPostRequest[types.Category](url, category)
+        if err != nil {
+            return nil, fmt.Errorf("Error during post request: %w", err)
+        }
+        createdCategories = append(createdCategories, *createdCategory)
+    }
+    return createdCategories, nil
 }
--- internal/api/createPlaces.go
+++ internal/api/createPlaces.go
@@ -1,13 +1,10 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 
 	"serpa-cli/internal/types"
+	"serpa-cli/internal/utils"
 )
 
 func CreatePlaces(baseUrl, apiVersion string, dbCategories []types.Category, csvPlaces []types.Place) ([]types.Place, error) {
@@ -20,44 +17,15 @@ func CreatePlaces(baseUrl, apiVersion string, dbCategories []types.Category, csv
 				break
 			}
 		}
-		jsonData, err := json.Marshal(place)
-		if err != nil {
-			return nil, fmt.Errorf("error marshalling place %v: %w", place.Name, err)
-		}
-
 		url := fmt.Sprintf("%s%s/place", baseUrl, apiVersion)
 
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+		createdPlace, err := utils.DoPostRequest[types.Place](url, place)
 		if err != nil {
-			return nil, fmt.Errorf("Error creating request: %v", err)
-		}
-		req.Header.Set("Content-Type", "application/json")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			return nil, fmt.Errorf("Error sending request: %v", err)
+			return nil, fmt.Errorf("Error during post request: %w", err)
 		}
 
-		body, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			return nil, fmt.Errorf("Error reading response body: %v", err)
-		}
-
-		if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("API returned non-success status: %d, body: %s", resp.StatusCode, string(body))
-		}
-
-		var createdPlace types.Place
-		err = json.Unmarshal(body, &createdPlace)
-		if err != nil {
-			return nil, fmt.Errorf("Error unmarshalling response: %v", err)
-		}
-
-		createdPlaces = append(createdPlaces, createdPlace)
+		createdPlaces = append(createdPlaces, *createdPlace)
 	}
-	
 
 	return createdPlaces, nil
 }
